90b2fd305820555f97ea2c17bedd510f48d3e3e2
Rishiverma-rv <85844449+Rishiverma-rv@users.noreply.github.com>
2023-08-18 23:55:28 +0530

    Add files via upload

---
+++ chatapp/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class Userform(UserCreationForm):
+    username=forms.CharField(widget=forms.TextInput(attrs={"class":"form-control","placeholder": "Enter userName"}))
+    email=forms.CharField(widget=forms.EmailInput(attrs={"class":"form-control","placeholder": "Enter email-address"}))
+    password1=forms.CharField(widget=forms.PasswordInput(attrs={"class":"form-control","placeholder": "Enter password"}))
+    password2=forms.CharField(widget=forms.PasswordInput(attrs={"class":"form-control","placeholder": "Confirm password"}))
+    class Meta:
+        model = User
+        fields = ['username', "email", "password1", "password2"]
